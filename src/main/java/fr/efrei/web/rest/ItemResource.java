0f420831d4b1ec8de86c4d7848527031d6ff179a
alexisnguyen01 <74011234+alexisnguyen01@users.noreply.github.com>
2023-11-29 09:33:12 +0100

    Update ItemResource.java

--- src/main/java/fr/efrei/web/rest/ItemResource.java
+++ src/main/java/fr/efrei/web/rest/ItemResource.java
@@ -1,15 +1,22 @@
 package fr.efrei.web.rest;
 
+import fr.efrei.web.domain.Item;
+import fr.efrei.web.service.ItemService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping ("/api")
+    
 public class ItemResource {
+    
     public final ItemService itemService;
-    public ItemRessource(ItemService itemservice) {this.itemService = ItemService;}
+    
+    public ItemRessource(ItemService itemservice) {this.itemService = itemService;}
+    
     @GetMapping ("/items")
-    public String test(){
-    public List <item> getALLItems() {return itemService.findAll();}
+    public List<item> getALLItems() {return itemService.findAll();}
 }
