164fc85e84f75db73ab7d09d820c9a993972d8db
alexisnguyen01 <74011234+alexisnguyen01@users.noreply.github.com>
2023-11-29 17:55:17 +0100

    DOcker

--- src/main/java/fr/efrei/server/web/rest/ItemResource.java
+++ src/main/java/fr/efrei/server/web/rest/ItemResource.java
@@ -2,12 +2,13 @@ package fr.efrei.server.web.rest;
 
 import fr.efrei.server.domain.Item;
 import fr.efrei.server.service.ItemService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api")
