f88a723ed474a881cc51d352e2807269873eef64
alexisnguyen01 <74011234+alexisnguyen01@users.noreply.github.com>
2023-12-10 22:36:46 +0100

    refactor the code

---
+++ src/main/java/fr/efrei/server/domain/Item.java
@@ -0,0 +1,42 @@
+package fr.efrei.server.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private Double price;
+
+    public Item() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+}
--- src/main/java/fr/efrei/server/web/rest/ItemResource.java
+++ src/main/java/fr/efrei/server/web/rest/ItemResource.java
@@ -1,15 +1,36 @@
 package fr.efrei.server.web.rest;
 
+import fr.efrei.server.domain.Item;
+import fr.efrei.server.service.ItemService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api")
-
 public class ItemResource {
+
+    public final ItemService itemService;
+
+    public ItemResource(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
     @GetMapping("/items")
-    public String test() {
-        return "Hello world";
+    public List<Item> getAllItems() {
+        return itemService.findAll();
+    }
+
+    @GetMapping("/items/{id}")
+    public Item getItem(@PathVariable String id) {
+//        log.debug("REST request to get Item : {}", id);
+        Item item =new Item();
+        item.setName("Pierre");
+        return item;
     }
 }
